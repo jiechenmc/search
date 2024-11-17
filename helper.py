ffd00c9e344476d96a20a636c8270fd76c80b710
Jie Chen <jiechenmc@gmail.com>
2024-11-16 21:54:00 -0500

    moving some things around

--- helper.py
+++ helper.py
@@ -1,12 +1,12 @@
 def write_checkpoint(checkpoint_id: int, worker: int):
     print(f"0 | Writing | {checkpoint_id} -> {worker}")
-    with open(f"checkpoint-{worker}.txt", "w") as f:
+    with open(f"./tmp/checkpoint-{worker}.txt", "w") as f:
         f.write(str(checkpoint_id))
 
 
 def read_checkpoint(worker: int):
     try:
-        with open(f"checkpoint-{worker}.txt", "r") as f:
+        with open(f"./tmp/checkpoint-{worker}.txt", "r") as f:
             checkpoint = int(f.read())
             return checkpoint
     except Exception:
@@ -14,10 +14,10 @@ def read_checkpoint(worker: int):
 
 
 def save_repo(url):
-    with open("out.txt", "a+") as f:
+    with open("./data/out.txt", "a+") as f:
         f.write("".join([url, "\n"]))
 
 
 def audit_repo(url):
-    with open("audit.txt", "a+") as f:
+    with open("./data/audit.txt", "a+") as f:
         f.write("".join([url, "\n"]))
